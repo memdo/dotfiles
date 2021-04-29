880d479b2204f12154591c521997e5255ab2c52f
M. Berkay Düzenli <memdo@protonmail.com>
2021-04-29 22:35:41 +0300

    Added dotfiles

---
+++ i3/dmenu.sh
@@ -0,0 +1,3 @@
+. "${HOME}/.cache/wal/colors.sh"
+
+dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "SF Mono-19"
---
+++ picom/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q picom
+
+# Wait until the processes have been shut down
+while pgrep -x picom >/dev/null; do sleep 1; done
+
+picom --experimental-backends --backend glx
