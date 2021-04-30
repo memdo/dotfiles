33136353ee9fa01125e25fee0d4a58a18577fde5
M. Berkay Düzenli <memdo@protonmail.com>
2021-04-30 17:57:34 +0300

    Little things

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+xrandr --newmode "1920x1080_75.50"  222.25  1920 2064 2264 2608  1080 1083 1088 1130 -hsync +vsync
 xrandr --addmode HDMI-A-0 "1920x1080_75.50"
 xrandr --output HDMI-A-0 --mode "1920x1080_75.50"
+
+nitrogen --restore &
 
 xrdb -load .Xresources
 
