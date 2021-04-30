42c80f6affac4ea1beb1930921d66183e8b1bbcc
M. Berkay Düzenli <memdo@protonmail.com>
2021-04-30 17:58:47 +0300

    Changing to bspwm

---
+++ bspwm/autostart.sh
@@ -0,0 +1,8 @@
+sh .config/polybar/launch.sh &
+# autokey-gtk &
+picom --experimental-backends --backend glx &
+numlockx &
+
+xrandr --newmode "1920x1080_75.50"  222.25  1920 2064 2264 2608  1080 1083 1088 1130 -hsync +vsync &
+xrandr --addmode --verbose HDMI-A-0 "1920x1080_75.50" &
+xrandr --output HDMI-A-0 --mode "1920x1080_75.50" &
